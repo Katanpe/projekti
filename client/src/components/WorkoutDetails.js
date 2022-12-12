01a13a556882516b674a9a77213e93141f4e0504
Darustet <58421506+Darustet@users.noreply.github.com>
2022-12-13 01:58:35 +0200

    Lamppu lisätty, on/off toiminto lisätty lamppuun

--- client/src/components/WorkoutDetails.js
+++ client/src/components/WorkoutDetails.js
@@ -1,5 +1,7 @@
 import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
 import { useAuthContext } from '../hooks/useAuthContext'
+import React, { useState } from 'react';
+import ToggleSwitch from "../components/ToggleSwitch";
 
 // date fns
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
@@ -9,6 +11,9 @@ const API_URL=process.env.REACT_APP_API_URL;
 const WorkoutDetails = ({ workout }) => {
   const { dispatch } = useWorkoutsContext()
   const { user } = useAuthContext()
+  let [lightSwitch, setLightSwitch] = useState(false);
+  let [bulb, setBulb] = useState("https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg")
+
 
   const handleClick = async () => {
     if (!user) {
@@ -28,6 +33,15 @@ const WorkoutDetails = ({ workout }) => {
     }
   }
 
+  const onLightSwitctChange = (checked) => {
+    setLightSwitch(checked);
+    if (!checked) {
+      setBulb("https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg")
+    } else {
+      setBulb("https://media.geeksforgeeks.org/wp-content/uploads/ONbulb.jpg")
+    }
+  };
+
   return (
     <div className="workout-details">
       <h4>{workout.title}</h4>
@@ -35,8 +49,22 @@ const WorkoutDetails = ({ workout }) => {
       <p><strong>Reps: </strong>{workout.reps}</p>
       <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
       <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+      <div>
+        <ToggleSwitch
+          id="lightSwitch"
+          checked={lightSwitch}
+          onChange={onLightSwitctChange}
+        />
+        <label htmlFor="lightSwitch">Turn lights on/off</label>
+      </div>
+      <div>
+        <img src={bulb} alt="lightbulb"/>
+        <p>LightSwitch: {String(lightSwitch)}</p>
+      </div>
     </div>
   )
 }
+
+
 
 export default WorkoutDetails
