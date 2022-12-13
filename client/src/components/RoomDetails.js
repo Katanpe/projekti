604bbe256b435af09368fd4d4f4e3a934f9552a0
Darustet <58421506+Darustet@users.noreply.github.com>
2022-12-13 10:45:19 +0200

    Deleted WorkoutDetails.js, moved everything to RoomDetails.js

--- client/src/components/RoomDetails.js
+++ client/src/components/RoomDetails.js
@@ -1,21 +1,26 @@
-import { useWorkoutsContext } from '../hooks/useWorkoutsContext'
+import { useRoomsContext as useRoomsContext } from '../hooks/useRoomsContext'
 import { useAuthContext } from '../hooks/useAuthContext'
+import React, { useState } from 'react';
+import ToggleSwitch from "../components/ToggleSwitch";
 
 // date fns
 import formatDistanceToNow from 'date-fns/formatDistanceToNow'
 
 const API_URL=process.env.REACT_APP_API_URL;
 
-const WorkoutDetails = ({ workout }) => {
-  const { dispatch } = useWorkoutsContext()
+const RoomDetails = ({ room }) => {
+  const { dispatch } = useRoomsContext()
   const { user } = useAuthContext()
+  let [lightSwitch, setLightSwitch] = useState(false);
+  let [bulb, setBulb] = useState("https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg")
+
 
   const handleClick = async () => {
     if (!user) {
       return
     }
 
-    const response = await fetch(API_URL  + '/workouts/' + workout._id, {
+    const response = await fetch(API_URL  + '/rooms/' + room._id, {
       method: 'DELETE',
       headers: {
         'Authorization': `Bearer ${user.token}`
@@ -24,19 +29,42 @@ const WorkoutDetails = ({ workout }) => {
     const json = await response.json()
 
     if (response.ok) {
-      dispatch({type: 'DELETE_WORKOUT', payload: json})
+      dispatch({type: 'DELETE_ROOM', payload: json})
     }
   }
 
+  const onLightSwitctChange = (checked) => {
+    setLightSwitch(checked);
+    if (!checked) {
+      setBulb("https://media.geeksforgeeks.org/wp-content/uploads/OFFbulb.jpg")
+    } else {
+      setBulb("https://media.geeksforgeeks.org/wp-content/uploads/ONbulb.jpg")
+    }
+  };
+
   return (
-    <div className="workout-details">
-      <h4>{workout.title}</h4>
-      <p><strong>Load (kg): </strong>{workout.load}</p>
-      <p><strong>Reps: </strong>{workout.reps}</p>
-      <p>{formatDistanceToNow(new Date(workout.createdAt), { addSuffix: true })}</p>
+    <div className="room-details">
+      <h4>{room.title}</h4>
+      <p><strong>Load (kg): </strong>{room.load}</p>
+      <p><strong>Reps: </strong>{room.reps}</p>
+      <p>{formatDistanceToNow(new Date(room.createdAt), { addSuffix: true })}</p>
       <span className="material-symbols-outlined" onClick={handleClick}>delete</span>
+      <div>
+        <ToggleSwitch
+          id="lightSwitch"
+          checked={lightSwitch}
+          onChange={onLightSwitctChange}
+        />
+        <label htmlFor="lightSwitch">Turn lights on/off</label>
+      </div>
+      <div>
+        <img src={bulb} alt="lightbulb"/>
+        <p>LightSwitch: {String(lightSwitch)}</p>
+      </div>
     </div>
   )
 }
 
-export default WorkoutDetails
+
+
+export default RoomDetails
